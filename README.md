5ec3f4742de1712b204359a97233378f4456b7d4
sbreault <60112611+sbreault@users.noreply.github.com>
2021-02-26 11:32:15 -0500

    update source

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # emailerservice
  Email service. Requires SMTP server (i.e. fakeSMTP)
+
+## Dependencies
+nodemailer - https://nodemailer.com/about/
+
+# FakeSMTP instance.
+To run FakeSMTP:
+Assuming you have $JAVA_HOME set.
+```shell script
+java -jar $FAKE_SMTP_HOME\fakeSMTP-2.0.jar
+```
+Config FakeSMTP to persist emails to a local directory.
+
+Config your service to use Host IP and port of your FakeSMTP instance.
+
+# config service with SMTP IP and port
+$SMTP_HOST='localhost';
+$SMTP_PORT=2525;
+
+## Running locally
+SMTP_HOST='127.0.0.1' SMTP_PORT=25 node index.js
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
+const http = require('http');
+const express = require('express');
+const app = express();
 const mailer = require('nodemailer');
  
-const HOST='localhost';
-const PORT=8080;
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const hostname = '127.0.0.1';
+const PORT=process.env.PORT || 8900;
+
+const SMTP_HOST=process.env.SMTP_HOST || '127.0.0.1';
+const SMTP_PORT=process.env.SMTP_PORT || 25;
+
+//console.log(process.env);
+
 
 const opentransport = mailer.createTransport({
-    host: HOST,
-    port: PORT
-});
+    host: SMTP_HOST,
+    port: SMTP_PORT
+})
 
 const securetransport = mailer.createTransport({
-    host: HOST,
-    port: PORT,
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    secure: true,
     auth: {
         user: 'ABC',
         pass: '123'
     }
-});
+})
 
 let transport = opentransport;
 
@@ -26,13 +40,6 @@ let textmessage = {
     text: 'I guess this works fine.'
 };
 
-//send text email
-transport.sendMail(textmessage, function(err, info) {
-    if(err) console.error(err);
-    else console.log(info);
-});
-
-
 let htmlmessage = {
     from: 'emailer_service@domain.com',
     to: 'breasteve@gmail.com',
@@ -40,12 +47,52 @@ let htmlmessage = {
     html: '<h1>You won!</h1><p>Congrats to you.'
 };
 
-//send html email
-transport.sendMail(htmlmessage, function(err, info) {
-    if(err) console.error(err);
-    else console.log(info);
+
+
+const server = http.createServer((req, res) => res.end('Running...'));
+
+server.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${PORT}/`);
 });
 
+
+//root
+app.get('/', function (req, res) {
+    res.end('success');
+})
+
+//send text email
+app.get('/sendtxt', function (req, res) {
+    let out;
+    transport.sendMail(textmessage, function(err, info) {
+        if(err){
+            console.error(err);
+            out = err;
+        }else{
+            console.log(info);
+            out = info;
+        }
+    });
+    res.end(out);
+})
+
+
+//send html email
+app.get('/sendhtml', function (req, res) {
+    let out;
+    transport.sendMail(htmlmessage, function(err, info) {
+        if(err){
+            console.error(err);
+            out = err;
+        }else{
+            console.log(info);
+            out = info;
+        }
+    });
+    res.end(out);
+})
+
+/*
 let attachmentmessage = {
     from: 'emailer_service@domain.com',
     to: 'breasteve@gmail.com',
@@ -59,3 +106,4 @@ transport.sendMail(attachmentmessage, function(err, info) {
     if(err) console.error(err);
     else console.log(info);
 });
+*/
